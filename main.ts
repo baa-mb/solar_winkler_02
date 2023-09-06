9df023ce18109ee92e580616285a8094d6fc4989
baa <34665120+baa-mb@users.noreply.github.com>
2023-09-06 15:53:20 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,5 +4,16 @@ let strip = neopixel.create(DigitalPin.P1, 3, NeoPixelMode.RGB)
 basic.forever(function () {
     zzz += 1
     basic.showNumber(zzz)
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
     basic.pause(2000)
+    strip.showColor(neopixel.colors(NeoPixelColors.Orange))
+    basic.pause(2000)
+    strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    basic.pause(1000)
+    for (let index = 0; index < 4; index++) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Black))
+        basic.pause(500)
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+        basic.pause(500)
+    }
 })
